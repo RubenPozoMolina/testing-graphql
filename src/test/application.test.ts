7139d01f58a0ddbd6c03d04bc4900e64cd803ed2
Rubén <rubenpozomolina@gmail.com>
2020-07-20 10:51:10 +0200

    Added eslint configuration and result verification

--- src/test/application.test.ts
+++ src/test/application.test.ts
@@ -1,22 +1,21 @@
-import request from "supertest";
-import express from "express";
-import { Application } from "../application"
+/* eslint-disable no-undef */
+import supertest from 'supertest'
+import { Application } from '../application'
 
-var app: Application;
+var app: Application
 
 describe('Simple graphql test', () => {
-
   beforeAll(() => {
     app = new Application()
-  });
-
-  test("Graphql query", async () => {
-    let result = await request(app.expressApplication)
-      .post("/graphql")
-      .send({ "query": "{\n  hello\n}", "variables": null })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200);
   })
 
+  test('Graphql query', async() => {
+    const result: supertest.Response = await supertest(app.expressApplication)
+      .post('/graphql')
+      .send({ query: '{\n  hello\n}', variables: null })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+    expect(result.text).toBe('{"data":{"hello":"Hello world!"}}')
+  })
 })
